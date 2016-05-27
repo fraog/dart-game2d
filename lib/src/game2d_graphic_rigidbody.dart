9b2fe01b899a0aef4ab0ab35358337d9d973c599
jesse <jessebrowne@gmail.com>
2016-05-27 16:50:24 -0700

    test

--- lib/src/game2d_graphic_rigidbody.dart
+++ lib/src/game2d_graphic_rigidbody.dart
@@ -6,11 +6,11 @@ class GraphicRigidBody implements Graphic {
 
   RigidBody2D body;
 
-  GraphicRect(this.body);
+  GraphicRigidBody(this.body);
 
   draw(CanvasRenderingContext2D ctx) {
     ctx.translate(this.body.x, this.body.y);
     ctx.rotate(this.body.rotation);
-    ctx.strokeRect(this.rect.left, this.rect.top, this.rect.w, this.rect.h);
+    ctx.strokeRect(this.body.left, this.body.top, this.body.w, this.body.h);
   }
 }
